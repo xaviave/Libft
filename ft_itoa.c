179ecc45e7b5b1d6956dfc3195b972d2429ec212
Xavier Martin <xamartin@z4r2p5.le-101.fr>
2017-11-13 16:17:06 +0100

    yeah

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,40 +6,46 @@
 /*   By: xamartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/08 17:41:55 by xamartin          #+#    #+#             */
-/*   Updated: 2017/11/13 15:50:46 by xamartin         ###   ########.fr       */
+/*   Updated: 2017/11/13 16:13:11 by xamartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_itoa(int n)
+static void		ft_is_negative(int *n, int *negative)
 {
-	char	mem;
-	char	*str;
-	size_t	i;
-	int		j;
-
-	i = 0;
-	j = n;
-	if (n < 0)
+	if (*n 	< 0)
 	{
-		mem = '-';
-		j = -j;
-		n = -n;
+		*n *= -1;
+		*negative = 1;
 	}
-	else
-		mem = '+';
-	while (j /= 10)
+}
+
+char			*ft_itoa(int n)
+{
+	int			mem;
+	int			i;
+	int			neg;
+	char		*str;
+
+	mem = n;
+	i = 2;
+	neg = 0;
+	ft_is_negative(&n, &neg);
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	while (mem /= 10)
 		i++;
-	(mem == '-') ? i++ : 0;
-	if (!(str = (char *)malloc(sizeof(char) * i)))
+	i += neg;
+	if (!(str = (char*)malloc(sizeof(char) * i)))
 		return (NULL);
-	str[i--] = '\0';
+	str[--i] = '\0';
 	while (i--)
 	{
-		str[i] = (n % 10) + 48;
+		str[i] = n % 10 + '0';
 		n = n / 10;
 	}
-	(mem == '-') ? str[0] = '-' : 0;
+	if (neg)
+		str[0] = '-';
 	return (str);
 }
